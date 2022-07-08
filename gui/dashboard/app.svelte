6241de18fe37bc6095b331da5d8e3e7c8045838e
Romein van Buren <romein@vburen.nl>
2022-07-08 14:50:15 +0200

    Dashboard aspect ratio
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- gui/dashboard/app.svelte
+++ gui/dashboard/app.svelte
@@ -1,1 +1,19 @@
-<h1>here comes the dashboard</h1>
+<script>
+  import AspectRatio from './components/aspectratio.svelte';
+</script>
+
+<AspectRatio>
+  <h1>here comes the dashboard</h1>
+</AspectRatio>
+
+<style>
+  :global(html, body) {
+    --radius: 10px;
+    --dark: #181818;
+
+    background-color: #000;
+    color: #fff;
+    padding: 0;
+    margin: 0;
+  }
+</style>
