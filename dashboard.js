f29f962de4d4661ae0ffc40fc17b3dbd80fc21ec
Romein van Buren <romein@vburen.nl>
2022-07-08 14:50:06 +0200

    Cleanup correctly
    Signed-off-by: Romein van Buren <romein@vburen.nl>

--- dashboard.js
+++ dashboard.js
@@ -5,7 +5,7 @@ const { svelte } = require('@sveltejs/vite-plugin-svelte');
 const fs = require('fs').promises;
 
 const build = (prefix = '') => viteBuild({
-  root: __dirname + '/gui/dashboard',
+  root: __dirname + '/gui/dashboard/',
   base: `${prefix}/statusdashboard/asset/`,
   plugins: [ svelte() ],
   build: {
@@ -20,7 +20,10 @@ const build = (prefix = '') => viteBuild({
   },
 });
 
-const cleanup = () => fs.rmdir(__dirname + '/gui/dashboard/build');
+const cleanup = () => fs.rm(__dirname + '/gui/dashboard/build', {
+  recursive: true,
+  force: true,
+});
 
 module.exports = { build, cleanup };
 build();
