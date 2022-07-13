670fe76249bf74dffe6e40a5e31384c943bd8ae3
Romein van Buren <romein@vburen.nl>
2022-07-13 11:16:07 +0200

    Module as link to dashboard

---
+++ gui/modules/webservicedashboard.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { createEventDispatcher, onMount } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  onMount(() => {
+    let url = window.location.href;
+    if (!url.endsWith('/')) url += '/';
+    url += 'status/dashboard';
+
+    window.open(url, '_blank').focus();
+    dispatch('close');
+  });
+</script>
